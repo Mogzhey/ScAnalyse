628a878108815d9c763e9b60dd4c682faa1b10d0
Mogzhey <mogzhey@yahoo.com>
2024-09-05 00:24:54 +0100

    Testing Qt window

--- src/test.cpp
+++ src/test.cpp
@@ -2,16 +2,32 @@
 #include <QMainWindow>
 #include <QLabel>
 #include <QString>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QSize>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    QMainWindow window;
-    window.setWindowTitle("Mainwindow");
+    QMainWindow mainWindow;
+    mainWindow.setWindowTitle("Mainwindow");
+    mainWindow.setMinimumSize(640, 480);
 
-    QLabel label("Hello World!", &window);
+    QWidget window(&mainWindow);
+    mainWindow.setCentralWidget(&window);
 
-    window.show();
+    QVBoxLayout layout(&window);
+    window.setLayout(&layout);
+
+    QLabel label1("Label 1");
+    QLabel label2("Label 2");
+    QLabel label3("Label 3");
+
+    layout.addWidget(&label1);
+    layout.addWidget(&label2);
+    layout.addWidget(&label3);
+
+    mainWindow.show();
     return app.exec();
 }
