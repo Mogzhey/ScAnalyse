482b8f43061d3a441203f1333f3480261933551c
Mogzhey <mogzhey@yahoo.com>
2024-09-04 22:44:30 +0100

    Got pybind11/embed to work

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <pybind11/embed.h> // everything needed for embedding
+namespace py = pybind11;
 
-int main()
-{
-	std::cout << "Hello World" << std::endl;
-	return 0;
+int main() {
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+
+    py::print("Hello, World!"); // use the Python API
 }
+
