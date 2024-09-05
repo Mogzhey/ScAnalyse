b20a7af9ac736eeed4d596a358fe73e34e644c37
Mogzhey <mogzhey@yahoo.com>
2024-09-05 14:10:07 +0100

    Create test plot

--- src/homescreen.cpp
+++ src/homescreen.cpp
@@ -9,14 +9,49 @@ HomeScreen::HomeScreen(QWidget* parent) : QWidget(parent)
 	this->setLayout(layout);
 
 	// Create labels
-	label11 = new QLabel("TL");
-	label12 = new QLabel("TR");
-	label21 = new QLabel("BL");
-	label22 = new QLabel("BR");
+	label11 = new QLabel("ROW1 COL1");
+	label21 = new QLabel("ROW2 COL1");
 
-	// Add labels to layout
+	// Create plot
+	plot = new QChartView();
+	createTestPlot(plot);
+
+	// Add labels and plot to layout
 	layout->addWidget(label11, 1, 1);
-	layout->addWidget(label12, 1, 2);
 	layout->addWidget(label21, 2, 1);
-	layout->addWidget(label22, 2, 2);
+	layout->addWidget(plot, 1, 2, 2, 1);
+}
+
+void HomeScreen::createTestPlot(QChartView* parent)
+{
+	// Create chart
+	auto chart = new QChart();
+	chart->setTitle("Test Plot");
+
+	// Create axes
+	auto xAxis = new QValueAxis();
+	xAxis->setTickCount(11);
+	xAxis->setTitleText("x values");
+
+	auto yAxis = new QValueAxis();
+	yAxis->setTickCount(11);
+	yAxis->setTitleText("y values");
+
+	// Create line of y = x^2
+	QLineSeries* series = new QLineSeries();
+	for (float x = -10.0f; x <= 10.0f; x += 0.2f)
+	{
+		series->append(x, x*x);
+	}
+	series->setName("y = x^2");
+
+	// Add line to chart
+	chart->addSeries(series);
+
+	// Add axes to chart
+	chart->setAxisX(xAxis, series);
+	chart->setAxisY(yAxis, series);
+
+	// Add chart to plot
+	plot->setChart(chart);
 }
--- src/homescreen.h
+++ src/homescreen.h
@@ -2,6 +2,11 @@
 #include <QGridLayout>
 #include <QLabel>
 
+#include <QChartView>
+#include <QChart>
+#include <QLineSeries>
+#include <QValueAxis>
+
 // TODO: Use QtCreator to generate .ui file instead of making the ui in code.
 class HomeScreen : public QWidget
 {
@@ -11,11 +16,14 @@ public:
 	HomeScreen(QWidget* parent = nullptr);
 
 private:
+	void createTestPlot(QChartView* parent);
+
+private:
 	QWidget* parent;
 	QGridLayout* layout;
 
 	QLabel* label11;
-	QLabel* label12;
 	QLabel* label21;
-	QLabel* label22;
+	
+	QChartView* plot;
 };
